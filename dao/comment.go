fd86cbc17d5f8bfa9dcc3e8fbffd667a43627c2c
Tompiano <2022214019@stu.cqupt.edu.cn>
2023-02-04 14:46:21 +0800

    api:修改评论接口中的数据库操作

--- dao/comment.go
+++ dao/comment.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InsertComment(m model.Comment) (err error) {
-	result, err := DB.Exec("insert into comment(userID,productID,parentID,content)value(?,?,?,?)",
+	result, err := DB.Exec("insert into comments(userID,productID,parentID,content)value(?,?,?,?)",
 		m.UserID, m.ProductID, m.ParentID, m.Content)
 	if err != nil {
 		log.Printf("when insert comment,mysql exec error:%v ", err)
@@ -18,7 +18,7 @@ func InsertComment(m model.Comment) (err error) {
 }
 
 func UpdateComment(commentID int, content string) (err error) {
-	_, err = DB.Exec("update comment set content=? where commentID=? ", content, commentID)
+	_, err = DB.Exec("update comments set content=? where commentID=? ", content, commentID)
 	if err != nil {
 		log.Printf("when delete comment,exec error:%v ", err)
 		return
